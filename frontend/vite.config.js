5c60432c14c3745b946ee10417a295ff4377dc00
aviral2806 <96320860+aviral2806@users.noreply.github.com>
2025-03-11 00:44:32 +0530

    Changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port: 5173,
+      host: true,
+      watch: {
+        usePolling: true,
+      },
+      esbuild: {
+        target: "esnext",
+        platform: "linux",
+      },
+    },
+    define: {
+      VITE_API_URL: JSON.stringify(env.VITE_API_URL),
+    },
+  };
+});
