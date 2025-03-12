60cb5e59807ac6cf4d21293ba908d8a353cd5f24
aviral2806 <96320860+aviral2806@users.noreply.github.com>
2025-03-13 01:47:07 +0530

    Changed api path, refactored code

--- todo-backend/src/main/java/com/aviral28/todo_backend/todo/TodoController.java
+++ todo-backend/src/main/java/com/aviral28/todo_backend/todo/TodoController.java
@@ -19,63 +19,54 @@ import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 public class TodoController {
-	private TodoRepository repo;
 	private TodoJPARepo jparepo;
+
 	@Autowired
 	public TodoController(TodoRepository repo, TodoJPARepo jparepo) {
 		super();
-		this.repo = repo;
 		this.jparepo = jparepo;
 	}
-	
-	@GetMapping(path="/todos")
-	public List<Todo> returnTodos(){
-//		return repo.getAllTodos();
+
+	@GetMapping(path = "/todos/")
+	public List<Todo> returnTodos() {
 		return jparepo.findAll();
 	}
-	
-	@PostMapping("/todos")
+
+	@PostMapping("/todos/")
 	public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
-//	    Todo createdTodo = repo.addTodo(todo);
-	    Todo newTodo = jparepo.save(todo);
-	    return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
+		Todo newTodo = jparepo.save(todo);
+		return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/todos/{id}")
-	public ResponseEntity<Todo> updateTodo(@PathVariable int id,@RequestBody Todo todo){
-//		Todo upd = repo.updTodo(id, todo);
-//		Todo updTcc = jparepo.getReferenceById(todo.getId());
-//		updT.setTaskDescription(todo.getTaskDescription());
-//		updT.setTaskName(todo.getTaskName());
-//		jparepo.save(updT);
+	public ResponseEntity<Todo> updateTodo(@PathVariable int id, @RequestBody Todo todo) {
 		Todo updT = jparepo.findById(id).map(
 				todo1 -> {
 					todo1.setTaskDescription(todo.getTaskDescription());
 					todo1.setTaskName(todo.getTaskName());
 					return jparepo.save(todo1);
 				}).orElse(null);
-		
+
 		return updT != null ? ResponseEntity.ok(updT) : ResponseEntity.notFound().build();
 	}
-	
+
 	@PatchMapping("/todos/{id}")
-	public ResponseEntity<Todo> toggleDone(@PathVariable int id){
-//		Todo upd = repo.toggleDone(id);
+	public ResponseEntity<Todo> toggleDone(@PathVariable int id) {
 		Todo updT = jparepo.findById(id).map(
 				todo -> {
 					todo.setDone(!todo.isDone());
 					return jparepo.save(todo);
 				}).orElse(null);
-				
+
 		return updT != null ? ResponseEntity.ok(updT) : ResponseEntity.notFound().build();
 	}
-	
+
 	@DeleteMapping("/todos/{id}")
-	public ResponseEntity<Todo> deleteTodo(@PathVariable int id){
-		if(jparepo.existsById(id)) {
+	public ResponseEntity<Todo> deleteTodo(@PathVariable int id) {
+		if (jparepo.existsById(id)) {
 			jparepo.deleteById(id);
 			return ResponseEntity.noContent().build();
-		}else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
